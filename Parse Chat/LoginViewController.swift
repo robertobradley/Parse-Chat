76b7ea621ca59c1124554ba2aa89cc05738b7973
Roberto Bradley <brad7746@m506-118-26432.local>
2018-09-28 09:53:04 -0700

    pushing for at end of class work

--- Parse Chat/LoginViewController.swift
+++ Parse Chat/LoginViewController.swift
@@ -9,28 +9,100 @@
 import UIKit
 import Parse
 
+
+
 class LoginViewController: UIViewController {
 
+//--OUTLETS--//
+@IBOutlet weak var usernameField: UITextField!
+@IBOutlet weak var passwordField: UITextField!
+
+//------VARIABLES------//
+//                       uialertcontroller
+let alertController = UIAlertController(title: "Title", message: "Message", preferredStyle: .alert)
+    
+let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+    { (action) in
+        // handle cancel response here. Doing nothing will dismiss the view.
+    }
+    
+let OKAction = UIAlertAction(title: "OK", style: .default)
+    { (action) in
+        // handle response here.
+    }
+
+    
+    //--OVERIDE FUNCTIONS--//
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        alertController.addAction(OKAction)
+        
+        
+       
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+//--FUNCTIONS--//
+    @IBAction func onSignIn(_ sender: Any)
+    {
+        if (usernameField.text == nil) || (passwordField.text == nil)
+        {
+            present(alertController, animated: true)
+        }
+        else {
+             loginUser()
+        }
+        
     }
-    */
+    
+    @IBAction func onSIgnUp(_ sender: Any)
+    {
+        if (usernameField.text == nil) || (passwordField.text == nil)
+        {
+            present(alertController, animated: true)
+        }
+        else {
+            registerUser()
+        }
+    }
+    
+    func registerUser() {
+        // initialize a user object
+        let newUser = PFUser()
+        
+        // set user properties
+        newUser.username = usernameField.text
+        newUser.password = passwordField.text
+        
+        // call sign up function on the object
+        newUser.signUpInBackground { (success: Bool, error: Error?) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                print("User Registered successfully")
+                // manually segue to logged in view
+            }
+        }
+    }
 
+    
+    func loginUser() {
+        
+        let username = usernameField.text ?? ""
+        let password = passwordField.text ?? ""
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user: PFUser?, error: Error?) in
+            if let error = error {
+                print("User log in failed: \(error.localizedDescription)")
+            } else {
+                print("User logged in successfully")
+                // display view controller that needs to shown after successful login
+            }
+        }
+    }
 }
